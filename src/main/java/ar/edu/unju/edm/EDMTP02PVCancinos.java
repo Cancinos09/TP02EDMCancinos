7d107bd2c879088f8a83a164028cf915f2c174cf
Mario <Mario@Pool-PC>
2021-04-15 16:54:32 -0300

    Trabajo Practico 2 Cancinos Mario

---
+++ src/main/java/ar/edu/unju/edm/EDMTP02PVCancinos.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDMTP02PVCancinos {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDMTP02PVCancinos.class, args);
+	}
+
+}
